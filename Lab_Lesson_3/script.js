b139cdc7d2d34b5260ddc71894818b3c016a45ae
Andrey <andrey_ivanov_sub@mail.ru>
2016-06-20 19:27:21 +0300

    commit_1

---
+++ Lab_Lesson_3/script.js
@@ -0,0 +1,90 @@
+/*
+var name= prompt("Введите ваше имя", "...");
+
+  if (name=="Andrey"){
+	  alert("Hello, Andrey");
+  } else {alert("?")}
+  var age= +prompt ("Введите возраст");
+  if (age==28){
+	  alert ("Вам 28");
+  } else if (age!==28){
+	  alert ("?");
+	  }
+ var result= name+":"+age;
+console.log(result);
+*/
+//var num = prompt("Вы еще здесь?") ? "Yes" : "No";
+//console.log(num);
+/*
+var x = 2;
+var st= 10;
+var result = 1;
+var i = 0;
+while (i<=st){
+	result*=x;
+	i++;
+	}
+	alert(result);
+*/
+/*
+var x =3;
+var st = 10;
+var result=1;
+var i = 0;
+do{
+	result*=x;
+	i++;
+}while (i<=st);
+alert(result);
+*/
+/*
+var x = 2;
+var st = 10;
+var result = 1;
+for (var i = 0; i<=st; i++){
+	result*=x;
+}
+alert(result);
+*/
+/*
+function sUm(x,y){
+var result = 1;
+for (var i = 0; i<=y; i++){
+	result*=x;
+}
+return result;
+}
+console.log(sUm(33,10));
+*/
+/*
+var book1={
+	title: "Titanik",
+	pubYear: 1977,
+	price: 2000
+};
+
+
+var book2={
+	title: "Sonnyc",
+	pubYear: 1999,
+	price: 2099
+};
+
+for (var cur in book1){
+	console.log(cur+":"+book1[cur]);
+};
+for (var cur in book2){
+	console.log(cur+":"+book2[cur]);
+};
+*/
+/*
+var a = [5, 12];
+var b = [];
+a[99] = 7;
+for (var cur in a){
+	b.push(a[cur]*a[cur])
+}
+
+//Записать в массив b квадраты значений массива a
+console.log(b);
+*/
